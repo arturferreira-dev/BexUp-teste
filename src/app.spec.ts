48d0d8fdd5a62d2f3f53a7d112a6724aa3473365
Artur Ferreira <arturferreira.dev@gmail.com>
2022-09-21 20:45:59 -0300

    fix: build

---
+++ src/app.spec.ts
@@ -0,0 +1,10 @@
+const soma = () => 2;
+
+describe("test", () => {
+  it("should be able to returns true", () => {
+    expect(soma()).toBe(2);
+  });
+  it("should be able to returns true", () => {
+    expect(false).toBe(false);
+  });
+});
---
+++ src/teste2/app2.spec.ts
@@ -0,0 +1,10 @@
+const soma2 = () => 2;
+
+describe("test", () => {
+  it("should be able to returns true", () => {
+    expect(soma2()).toBe(2);
+  });
+  it("should be able to returns true", () => {
+    expect(false).toBe(false);
+  });
+});
